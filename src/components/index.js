ae215859681ba5d7e9084854f013f7a3a85f25cf
eliveli <60069112+eliveli@users.noreply.github.com>
2022-01-20 21:45:57 +0900

    최초 골격 잡기

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Contact from "./Contact";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Project from "./Project";
+import Skill from "./Skill";
+
+export {Contact, Home, Navbar, Project, Skill};
