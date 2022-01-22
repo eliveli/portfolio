7066fac0581e66f801e3f35975f146cf0f16bd30
eliveli <60069112+eliveli@users.noreply.github.com>
2022-01-23 01:47:28 +0900

    Design: 프로젝트 섹션 화면 구성

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import Text from '../elements/Text';
+import Text from './elements/Text';
 import styled from "styled-components";
 
-export default function Project() {
-  return (
+const Project = () => (
   <Section>
       <Text isTittle={true}>Projects</Text>
       <Text>My projects are ...</Text>
-  </Section>);
-}
+      <ImgContainer>
+        <img width="100%" src="https://cdn.pixabay.com/photo/2020/02/04/17/07/drink-4818863_960_720.jpg" alt="project" />
+        <img width="100%" src="https://cdn.pixabay.com/photo/2020/02/04/17/07/drink-4818863_960_720.jpg" alt="project" />
+        <img width="100%" src="https://cdn.pixabay.com/photo/2020/02/04/17/07/drink-4818863_960_720.jpg" alt="project" />
+        <img width="100%" src="https://cdn.pixabay.com/photo/2020/02/04/17/07/drink-4818863_960_720.jpg" alt="project" />
+      </ImgContainer>
+  </Section>
+  );
+
+export default Project;
 
 const Section = styled.section`
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
+
+    width: 100%;
+`
+const ImgContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    border: 1px solid black;
+    gap: 20px;
+    padding: 20px;
+
+    @media only screen and (max-width: 760px) {
+        grid-template-columns: 1fr;
+    }
 `
