1604952f0445ce63ae03cb0673362eee456bb03c
eliveli <60069112+eliveli@users.noreply.github.com>
2022-01-23 16:34:02 +0900

    Feat: 화면 너비에 따른 아이콘 간격/크기 설정 변경
    - 플랫폼 별 연락처 아이콘 : 화면 별 고정값(media screen 설정) 삭제, flex 세부 설정(width, justify-content 등) 변경
    - 깃허브 아이콘 : media screen 설정 추가. 작은 화면에 작은 아이콘 설정

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,8 +10,8 @@ export default function Contact() {
             
             {/* 깃허브 링크 */}
             go to 
-            <a href="https://github.com/eliveli" target="_blank" rel="noreferrer">
-                <span style={{color:"#444"}} class="iconify" data-icon="uiw:github" data-width="50" data-height="50"></span>
+            <a href="https://github.com/eliveli" target="_blank" rel="noopener noreferrer">
+                <GithubIcon className="iconify" data-icon="uiw:github"></GithubIcon>
             </a>
 
             {/* 플랫폼별 연락처 */}
@@ -28,20 +28,35 @@ const Section = styled.section`
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
+
+    width: 100%;
+    /* 상위 element부터 width 설정해야 하위 element에 width % 로 설정 가능. */
+`
+const GithubIcon = styled.span`
+    width: 50px;
+    height: auto;
+    
+    color: #444;
+
+    /* 화면 너비 작을 때 아이콘 크기 조정 */
+    @media only screen and (max-width: 360px) {
+        width: 40px;
+    }
 `
 const ContactInfoContainer = styled.div`
     display: flex;
+    justify-content: space-evenly;
+    /* flex container 설정 */
+    /* 화면 resize 될 때(반응형) 각 flex item 간격/배치 조정 */
+    /* (유의) 이 때 gap을 적용하면 여기에서 추가로 item 간격이 들어가는 것 */
+
     margin-top: 20px;
     margin-bottom: 30px;
-
-    /* 태블릿사이즈부터 아이콘 별 간격 설정 */
-    @media only screen and (min-width: 768px) {
-        gap: 80px;
-    }
+    width: 100%;
 `
 
 
-
+// 각 연락처 플랫폼
 const ContactItem = ({dataIcon,platform}) => {
     return (
         <SkillItemContainer>
@@ -50,17 +65,6 @@ const ContactItem = ({dataIcon,platform}) => {
         </SkillItemContainer>
     );
 }
-
-const PlatformIcon = styled.span`
-    width:65px;
-    height:65px;
-    color: #666;
-
-    @media only screen and (min-width: 768px) {
-        width:100px;
-        height:100px;
-    }
-`
 
 ContactItem.defaultProps = {
     dataIcon: "",
@@ -72,7 +76,19 @@ const SkillItemContainer = styled.div`
     flex-direction: column;
     align-items: center;
     
-    width: 100px;
+    width: 20%;
+    min-width: 65px;
+    max-width: 100px;
+    /* 상위 element(flex container)에 대해 flex item으로써 width 설정 */
+    /* width %로 적용 시 부모에 대한 비율/형제와의 간격을 고려 */
+    /* (유의) width 100%를 넣으면 상위 element에 주었던 justify content 정렬이 안 먹힘 */
+`
+const PlatformIcon = styled.span`
+    width:100%;
+    height:auto;
+    color: #666;
+    /* api로 받아 와 element에 적용하는 아이콘도 width % 설정 가능 */
+    /* (유의) 부모/조상 element의 width를 명확히 설정해야 함 */
 `
 const SkillName = styled.span`
     width: 100%;
